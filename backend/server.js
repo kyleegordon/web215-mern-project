4deb30ebd4436db5bae6625fa2ff97e5bb227992
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-04-21 11:20:02 -0400

    attempt to fix port issue

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const path = require('path')
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const connectDB = require('./config/db');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const port = process.env.PORT || 5000;
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
 
 connectDB();
 
